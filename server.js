f42df4f859838297351226bb4a81b437c6aac86e
TSDPRASAD88 <prasad.thamarana2006@gmail.com>
2025-03-04 15:15:55 +0530

    GG

--- server.js
+++ server.js
@@ -33,6 +33,27 @@ Example Responses:
 Add the required logic below to complete the API.
 */
 
+app.get("/assistant/greet",(req,res)=>{
+  try{
+     const query=req.query;
+     if(!query.name){
+      res.status(400).send({message:"please include name in url as query"});
+     }
+     const day= new Date();
+     console.log(day.getDay());
+     const dayArray=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+     
+     console.log(query);
+     res.status(200).send({
+  welcomeMessage: `Hello, ${query.name}! Welcome to our assistant app!`,
+  dayMessage: `Happy ${dayArray[day.getDay()]}! Start your week with energy!`
+});
+  }catch(error){
+    res.status(500).send({message:"something went wrong",error:error});
+  }
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
